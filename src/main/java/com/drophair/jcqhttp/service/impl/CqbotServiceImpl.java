700180a4dad94727a001b23ccad947383abe4d9e
drophair <867301687@qq.com>
2023-05-29 10:55:15 +0800

    补充提交

--- src/main/java/com/drophair/jcqhttp/service/impl/CqbotServiceImpl.java
+++ src/main/java/com/drophair/jcqhttp/service/impl/CqbotServiceImpl.java
@@ -2,5 +2,11 @@ package com.drophair.jcqhttp.service.impl;
 
 import com.drophair.jcqhttp.service.ICqbotService;
 
+import javax.servlet.http.HttpServletRequest;
+
 public class CqbotServiceImpl implements ICqbotService {
+    @Override
+    public void cqbotEvenHandle(HttpServletRequest request) {
+        
+    }
 }
