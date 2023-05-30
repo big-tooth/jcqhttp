9269c2c43b6047a885d1e9d7df040baa77f819c7
big-tooth <867301687@qq.com>
2023-05-30 23:27:20 +0800

    feat(反向HTTP): 反向HTTP接口调通
    反向HTTP接口调通

--- src/main/java/com/drophair/jcqhttp/service/impl/CqbotServiceImpl.java
+++ src/main/java/com/drophair/jcqhttp/service/impl/CqbotServiceImpl.java
@@ -1,12 +1,46 @@
 package com.drophair.jcqhttp.service.impl;
 
+import com.alibaba.fastjson2.JSONObject;
 import com.drophair.jcqhttp.service.ICqbotService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.thymeleaf.util.StringUtils;
 
 import javax.servlet.http.HttpServletRequest;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
+@Slf4j
+@Service
 public class CqbotServiceImpl implements ICqbotService {
     @Override
     public void cqbotEvenHandle(HttpServletRequest request) {
+        JSONObject jsonParam = this.getJSONParam(request);
+//        log.info("接收参数为:{}",jsonParam.toString()/* !=null ? "SUCCESS" : "FALSE"*/);
+        if("message".equals(jsonParam.getString("post_type"))){
+            String message = jsonParam.getString("message");
+            if (!StringUtils.isEmpty(message)) {
+                log.info("接收参数为:{}",jsonParam.toString());
+            }
+        }
+    }
 
+    public JSONObject getJSONParam(HttpServletRequest request){
+        JSONObject jsonParam = null;
+        try {
+            // 获取输入流
+            BufferedReader streamReader = new BufferedReader(new InputStreamReader(request.getInputStream(), "UTF-8"));
+
+            // 数据写入Stringbuilder
+            StringBuilder sb = new StringBuilder();
+            String line = null;
+            while ((line = streamReader.readLine()) != null) {
+                sb.append(line);
+            }
+            jsonParam = JSONObject.parseObject(sb.toString());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return jsonParam;
     }
 }
