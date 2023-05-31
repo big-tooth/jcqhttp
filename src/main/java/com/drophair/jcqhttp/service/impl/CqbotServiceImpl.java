d5609447bc84de25c5310fe65ac37bd9f042bbf1
big-tooth <867301687@qq.com>
2023-05-31 21:28:43 +0800

    fix(target): maven 打包找不到主类
    maven 打包找不到主类

--- src/main/java/com/drophair/jcqhttp/service/impl/CqbotServiceImpl.java
+++ src/main/java/com/drophair/jcqhttp/service/impl/CqbotServiceImpl.java
@@ -4,7 +4,7 @@ import com.alibaba.fastjson2.JSONObject;
 import com.drophair.jcqhttp.service.ICqbotService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-import org.thymeleaf.util.StringUtils;
+import org.springframework.util.StringUtils;
 
 import javax.servlet.http.HttpServletRequest;
 import java.io.BufferedReader;
