8671052116c37a36782b078d9a21cc2ea41bda04
big-tooth <867301687@qq.com>
2023-06-06 22:49:21 +0800

    feat(dao): 完成部分dao和service
    dao
    	- Msg
    service
    	- IFriendService

---
+++ src/main/java/com/drophair/jcqhttp/dao/Msg.java
@@ -0,0 +1,30 @@
+package com.drophair.jcqhttp.dao;
+
+public class Msg {
+
+    private int user_id;
+
+    private int group_id;
+
+    private String message;
+
+    private boolean auto_escape;
+
+    private int message_id;
+
+    private String message_type;
+
+    private boolean group;
+
+    private int real_id;
+
+    private Object sender;
+
+    private int time;
+
+    private String raw_message;
+
+    private String nickname;
+
+    private int user_id;
+}
--- src/main/java/com/drophair/jcqhttp/service/IFriendService.java
+++ src/main/java/com/drophair/jcqhttp/service/IFriendService.java
@@ -1,4 +1,21 @@
 package com.drophair.jcqhttp.service;
 
+import com.drophair.jcqhttp.dao.Friend;
+import com.drophair.jcqhttp.dao.Msg;
+
 public interface IFriendService {
+
+    public String get_stranger_info(Friend friend);
+
+    public String get_frient_list();
+
+    public String get_unidirectional_friend_list();
+
+    public void delete_friend(Friend friend);
+
+    public void delete_unidirectional_friend(Friend friend);
+
+    public String send_private_msg(Msg message);
+
+    public String send_group_msg(Msg message);
 }
